f668e05287f3be9443c35751e6bcb6e0f3315342
Michael Fenwick <mickyfen17@aol.com>
2016-12-06 00:03:30 -0700

    Add helper function to style font size on win

--- app.js
+++ app.js
@@ -27,30 +27,34 @@ function disableClearBtn() {
   }
 }
 
+// Clear button function
+clearBtn.addEventListener("click", function() {
+  numberGuessInput.value = "";
+  disableClearBtn();
+});
+
 // Load random number on page load
 window.onload = function() {
   randomNumber = Math.ceil(Math.random() * 100);
   textVisible();
   disableClearBtn();
+  typeOfInput();
   console.log(randomNumber);
 };
 
 // Load random number on reset
 resetBtn.addEventListener("click", function() {
   randomNumber = Math.ceil(Math.random() * 100);
+  guessText.className = "normalText";
   lastGuessNumber.innerText = "Take a guess...";
   numberGuessInput.value = "";
+  lastGuessNumber.style.color = "#ed5a64";
+  typeOfInput();
   textVisible();
   disableClearBtn();
   resetBtn.disabled = true;
   guessBtn.disabled = false;
   console.log(randomNumber);
-});
-
-// Clear button function
-clearBtn.addEventListener("click", function() {
-  numberGuessInput.value = "";
-  disableClearBtn();
 });
 
 // Guess function
@@ -66,20 +70,32 @@ guessBtn.addEventListener("click", function() {
   guessText.style.visibility = "visible";
   lastGuessWas.style.visibility = "visible";
 
-  console.log(userNumber + " " + randomNumber);
-
   if (userNumber === randomNumber) {
-    console.log("BOOM!");
-    guessText.innerText = "BOOM!";
+    boom();
     lastGuessNumber.innerText = userNumber;
+    lastGuessNumber.style.color = "#1abc9c";
     guessBtn.disabled = true;
   } else if (userNumber > randomNumber) {
-    console.log("That is too high");
     guessText.innerText = "That is too high";
     lastGuessNumber.innerText = userNumber;
   } else {
-    console.log("That is too low...");
     guessText.innerText = "That is too low";
     lastGuessNumber.innerText = userNumber;
   }
+  typeOfInput();
 });
+
+// Function to add boom class when answer is correct
+function boom() {
+  guessText.innerText = "BOOM!";
+  guessText.className = "boom";
+}
+
+// Function to set size depending on guess inner test
+function typeOfInput() {
+  if(lastGuessNumber.innerText === "Take a guess...") {
+    lastGuessNumber.style.fontSize = "2em";
+  } else {
+    lastGuessNumber.style.fontSize = "7em";
+  }
+}
